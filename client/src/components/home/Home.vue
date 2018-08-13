8a74a70427d8c2d7c786343d585e0dbed2f05b0f
cyrilpluche <cyril.pluche@etu.umontpellier.fr>
2018-08-13 19:24:20 +0200

    [vue] - Top bar 50%

--- client/src/components/home/Home.vue
+++ client/src/components/home/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container>
+  <v-container id="homepage">
     home
   </v-container>
 </template>
--- client/src/components/topbar/Topbar.vue
+++ client/src/components/topbar/Topbar.vue
@@ -1,7 +1,43 @@
 <template>
-    <v-container id="topbar">
-      Ok boy
-    </v-container>
+  <v-container grid-list-md text-xs-center id="topbar">
+    <v-layout row wrap justify-center>
+      <v-flex xs2>
+        <v-card>
+          <img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg">
+          <hr>
+          <h2>J'ai Soif</h2>
+        </v-card>
+      </v-flex>
+      <v-flex xs2>
+        <v-card>
+          <img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg">
+          <hr>
+          <h2>Les Bails</h2>
+        </v-card>
+      </v-flex>
+      <v-flex xs2>
+        <v-card>
+          <img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg">
+          <hr>
+          <h3>Adopte Un Pote</h3>
+        </v-card>
+      </v-flex>
+      <v-flex xs2>
+        <v-card>
+          <img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg">
+          <hr>
+          <h3>Ma Grosse Tête</h3>
+        </v-card>
+      </v-flex>
+      <v-flex xs2>
+        <v-card>
+          <img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg">
+          <hr>
+          <h2>Mes Crew</h2>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
