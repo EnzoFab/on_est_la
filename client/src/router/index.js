583d901b474e420cfd7f9bd9caf880c15ef1e7d6
cyrilpluche <cyril.pluche@etu.umontpellier.fr>
2018-08-13 14:20:05 +0200

    [vue] - router navbar ERROR

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Navbar from '@/components/navbar/Navbar'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+
+    {
+      path: '/navbar',
+      name: 'Navbar',
+      component: Navbar
     }
   ]
 })
